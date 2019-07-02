b76a11102d77be20a31f58126fb779397971539d
Louis C. Murguia <louiscmm@gmail.com>
2019-07-01 20:38:51 -0500

    Enhancement of box-muller.c

--- box-muller.c
+++ box-muller.c
@@ -11,93 +11,56 @@
  *      Date        Programmer          Description of change
  *      ====        ==========          =======================
  *      21/06/19    Louis C. Murguía    Original Code.
+ *      01/07/19    Louis C. Murguía    No saving data in .txt.
+ *                                      Enhancement of function
+ *                                      randomBM.
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
-#include <stdbool.h>
-#include <errno.h>
-#include <string.h>
 
 /*  Declaration of functions:
  *      randomBM
  *      */
-double * randomBM();
-double getNumber();
+void randomBM(double numbers[], int size);
 
 #define PI 3.1415926535898
 #define n 10000    // Number of values to generate
 
-// Error number
-extern int errno;
-
 int main ()
 {
     // Data dictionary: declare variable types & definitions
-    double *pBM;            // Pointer to array of random values
-                            // obtain by randomBM
-    FILE *fp;               // Pointer to file (uniform dist)
-    FILE *fpBM;             // Pointer to file (normal dist)
-    bool opened = false;    // Boolean value to verify if the
-                            // file opened (uniform dist)
-    bool openedBM = false;  // Boolean value to verify if the
-                            // file opened (normal dist)
-    int status = 0;         // I/O status
-    char msg[80];           // Error message
+    double valuesBM[n];     // Array of random values obtained
+                            // by randomBM
 
-    /**************************************************************/
+    /**********************************************************/
 
-    // Open/create the files, and check for errors on open.
-    opened = ( fp = fopen("rndUniform.txt", "w") );
-    openedBM = ( fpBM = fopen("rndNormal.txt", "w") );
+    // Set the seed
+    srand48(time(NULL));
 
-    if (opened == true && openedBM == true) {
-        // Files opened successfully.
-
-        // Set the seed
-        srand48(time(NULL));
-
-        // Get the random values using Box-Muller Transform
-        pBM = randomBM();
-
-        // Save the values in files
-        for (int i = 0; i < n; ++i) {
-            fprintf(fpBM, "%lf\n", pBM[i]);
-            fprintf(fp, "%lf\n", drand48());
-        }
-
-    } else {  // File did not opened. An error occurred.
-         status = errno;
-         strcpy(msg, strerror(status));
-         fprintf(stderr,
-                 "Can't open the file: %s\n", msg);
-         exit(status);
-    }
-
-     // Close files
-    fclose(fp);
-    fclose(fpBM);
+    // Get the random values using Box-Muller Transform
+    randomBM(valuesBM, n);
 
     return 0;
 }
 
 /**************************************************************/
 
-double * randomBM() {
+void randomBM(double numbers[], int size) {
     /*
      * Purpose:
-     *  To generate n-random values according to Box & Muller.
+     *  To generate random values according to Box & Muller.
      */
 
     // Data dictionary: declare variable types & definitions
-    static double numbers[n];   // Array of random numbers
-                                // using BM Transform
-    double x1, x2;              // Uniformly random values
+    // double numbers[size] -->    Array of random numbers
+    //                             using BM Transform
+    double x1, x2;              // Random numbers (uniform dist)
 
     // Box-Muller Transform
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < size; ++i) {
         // Generate two random values uniformly distributed
         // between 0 and 1
         x1 = drand48();
@@ -106,7 +69,5 @@ double * randomBM() {
         // Generate the BM-random value
         numbers[i] = ( sqrt(-2*log(x1)) * cos(2*PI*x2) );
     }
-
-    return numbers;
 
 }
