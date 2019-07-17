b0fc1219d6f47642cb4a54d1453c636f152fad37
Louis C. Murguia <louiscmm@gmail.com>
2019-07-17 17:36:25 -0500

    Unnecessary folders removed

--- Intel-C/DerivedData/Intel-C/Build/Intermediates.noindex/Intel-C.build/Debug/Intel-C.build/Objects-normal/x86_64/mean_square_displacement.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
- /Users/KSHMR/Dropbox/Maestría/Computo\ Cientifico\ de\ Alto\ Rendimiento/Códigos/C\ Language/Intel-C/Intel-C/mean_square_displacement.c \
- /Users/KSHMR/Dropbox/Maestría/Computo\ Cientifico\ de\ Alto\ Rendimiento/Códigos/C\ Language/Intel-C/Intel-C/rndbm.h
--- Intel-C/DerivedData/Intel-C/Build/Intermediates.noindex/Intel-C.build/Debug/Intel-C.build/Objects-normal/x86_64/rndbm.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
- /Users/KSHMR/Dropbox/Maestría/Computo\ Cientifico\ de\ Alto\ Rendimiento/Códigos/C\ Language/Intel-C/Intel-C/rndbm.c \
- /Users/KSHMR/Dropbox/Maestría/Computo\ Cientifico\ de\ Alto\ Rendimiento/Códigos/C\ Language/Intel-C/Intel-C/rndbm.h
